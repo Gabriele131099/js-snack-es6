6a333e5846f92d552ec71619ed85a427bd458b67
gabriele131099 <gabrisanna131099@gmail.com>
2021-06-18 17:26:19 +0200

    struttura

--- es 3/script.js
+++ es 3/script.js
@@ -12,38 +12,40 @@ const clubs = [
         falli : 0 
       }   
   ]
-  let falliTanti;  // qui metterò l'oggetto club
-  let moreFalli = 0; 
+  let nomeOggetto;  // qui metterò l'oggetto club
+  let falliMax = 0; 
   const stampa = document.getElementById('stampa') 
   let variabileRandom = ''
   const numRandom = (min,max) => Math.floor(Math.random()*max - min +1)+min
-  for (let i = 0; i < clubs.length; i++) { 
+for (let i = 0; i < clubs.length; i++) { 
     clubs[i].punti = numRandom(0,100)
     clubs[i].falli = numRandom(0,10)
-      const {falli} = clubs[i];
-      if (falli > moreFalli) {
-        falliTanti = clubs[i];
-        moreFalli = falli;
-      }      
-    }
+    const {falli} = clubs[i];
+    if (falli > falliMax) {
+      nomeOggetto = clubs[i];
+      falliMax = falli;
+    }      
+    console.log(falli)
+}
+
 for (let i = 0; i < clubs.length; i++) {
-  if (clubs[i].falli < moreFalli) {
+  if (clubs[i].falli < falliMax) {
     variabileRandom += '<li>club: '   
     variabileRandom += clubs[i]['nome'] + ', ' 
-    variabileRandom += clubs[i]['falli'] + ', '  
     variabileRandom += clubs[i]['punti'] + ', '
+    variabileRandom += clubs[i]['falli'] + ', '  
     variabileRandom += '</li>' 
   } else {
     variabileRandom += '<li  style="background-color: yellow;">club: '  
     variabileRandom += clubs[i]['nome'] + ', ' 
-    variabileRandom += clubs[i]['falli'] + ', '  
     variabileRandom += clubs[i]['punti'] + ', '
+    variabileRandom += clubs[i]['falli'] + ', '  
     variabileRandom += '</li>'
   }
 }
   
 
-console.log(moreFalli)
+console.log(falliMax)
 stampa.innerHTML = variabileRandom
 
 
