3f1b0a129067762307599a4a354f14dc8a0280af
gabriele131099 <gabrisanna131099@gmail.com>
2021-06-18 17:09:33 +0200

    struttura

--- es 3/script.js
+++ es 3/script.js
@@ -12,30 +12,38 @@ const clubs = [
         falli : 0 
       }   
   ]
-let variabileRandom = ''
-let falliTanti;  // qui metterò l'oggetto club
-let moreFalli = 0; // qui metterò solo l'età dello studente più anziano mai visto
-const stampa = document.getElementById('stampa') 
-const numRandom = (min,max) => Math.floor(Math.random()*max - min +1)+min
-for (let i = 0; i < clubs.length; i++) { 
-  clubs[i].punti = numRandom(0,100)
-  clubs[i].falli = numRandom(0,10)
-  const {falli} = clubs[i];
-  if (falli > moreFalli) {
-    falliTanti = clubs[i];
-    moreFalli = falli;
-  }
-  if (clubs[i].falli === moreFalli) {
-    variabileRandom += '<li  style="background-color: yellow;">club: '   
-  } else {
+  let falliTanti;  // qui metterò l'oggetto club
+  let moreFalli = 0; 
+  const stampa = document.getElementById('stampa') 
+  let variabileRandom = ''
+  const numRandom = (min,max) => Math.floor(Math.random()*max - min +1)+min
+  for (let i = 0; i < clubs.length; i++) { 
+    clubs[i].punti = numRandom(0,100)
+    clubs[i].falli = numRandom(0,10)
+      const {falli} = clubs[i];
+      if (falli > moreFalli) {
+        falliTanti = clubs[i];
+        moreFalli = falli;
+      }      
+    }
+for (let i = 0; i < clubs.length; i++) {
+  if (clubs[i].falli < moreFalli) {
     variabileRandom += '<li>club: '   
-  }
+    variabileRandom += clubs[i]['nome'] + ', ' 
+    variabileRandom += clubs[i]['falli'] + ', '  
+    variabileRandom += clubs[i]['punti'] + ', '
+    variabileRandom += '</li>' 
+  } else {
+    variabileRandom += '<li  style="background-color: yellow;">club: '  
     variabileRandom += clubs[i]['nome'] + ', ' 
     variabileRandom += clubs[i]['falli'] + ', '  
     variabileRandom += clubs[i]['punti'] + ', '
     variabileRandom += '</li>'
+  }
 }
+  
 
+console.log(moreFalli)
 stampa.innerHTML = variabileRandom
 
 
